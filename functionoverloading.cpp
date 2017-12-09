b26926d078af2f7a99be33f38dac775d779b3a2f
dinesh <dineshsmart101.dm@gmail.com>
2017-12-09 05:08:54 -0800

    Update functionoverloading.cpp

--- functionoverloading.cpp
+++ functionoverloading.cpp
@@ -1,40 +1,19 @@
-#include <iostream>
-
-using namespace std;
-
-class circle
-{
-    private:
-       double area=0;
-    public:
+                     /* Without OOP concept */
 
     double calculateArea(double breadth , double height)
     {
         area = 1/3 * breadth * height;
     }
-    double printArea()
-    {
-        return area;
-    }
     double calculateArea(double radius)
     {
         area = 3.14 * radius * radius;
     }
-    double printArea1()
-    {
-        // cout<<"Area of the circle : "<<area<<endl;
-      return area;
+ 
         
-    }
-};
+    
 int main()
 {
-    circle ball;
-    ball.calculateArea(2,2);
-    ball.printArea();
-    cout<<"Area of the circle : "<<area<<endl;
-    ball.calculateArea(2);
-    ball.printArea1();
-    cout<<"Area of the recatangle :"<<area;
+    cout<<"Area of the circle : "<<  ball.calculateArea(2,2)<<endl;
+    cout<<"Area of the recatangle :"<<ball.calculateArea(2);
     return 1;
 }
